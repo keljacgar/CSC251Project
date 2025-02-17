65b9dae096282cefbe46a45fd513e1a1dad64c35
Kelly <kgarci50@lab.cpcc.edu>
2025-02-17 14:47:18 -0500

    Update Policy.java

--- Policy.java
+++ Policy.java
@@ -95,19 +95,3 @@ public class Policy
          
       return price;
    }
-   
-   // Display all information about the policy
-   public void displayInformation()
-   {
-      System.out.println("Policy Number: " + policyNumber);
-      System.out.println("Provider Name: " + providerName);
-      System.out.println("Policyholder's First Name: " + firstName);
-      System.out.println("Policyholder's Last Name: " + lastName);
-      System.out.println("Policyholder's Age: " + age);
-      System.out.println("Policyholder's Smoking Status (Y/N): " + smokingStatus);
-      System.out.println("Policyholder's Height: " + height + " inches");
-      System.out.println("Policyholder's Weight: " + weight + " pounds");
-      System.out.printf("Policyholder's BMI: %.2f\n", getBMI());
-      System.out.printf("Policy Price: $%.2f\n", getPrice());
-   }
-}
