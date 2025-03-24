a7c0c18e9c2c007393bdd82c3e690e4232e3de23
Kelly <kgarci50@lab.cpcc.edu>
2025-03-24 15:35:47 -0400

    Update Project_Kelly_Garcia.java
    Modified to read input from a file

--- Project_Kelly_Garcia.java
+++ Project_Kelly_Garcia.java
@@ -1,59 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import Java.util.ArrayList
 import java.util.Scanner;
 
-public class Project_Kelly_Garcia 
-{   
-   public static void main(String[] args)
-   {
-      String policyNumber;
-      String providerName;
-      String firstName;
-      String lastName;
-      int age;
-      String smokingStatus;
-      double height;
-      double weight;
+public class Project_Kelly_Garcia {   
+   public static void main(String[] args) {
+   ArrayList<Policy> policies = new Array<>();
+   
+   try (Scanner fileScanner = new Scanner(new File("PolicyInformation.txt"))) {
+      while (fileScanner.hasNextLine()) {
+      String policyNumber = fileScanner.next();
+      String providerName= fileScanner.next();
+      String firstName = fileScanner.next();
+      String lastName = fileScanner.next();
+      int age = Integer.parseInt(fileScanner.next());
+      String smokingStatus = fileScanner.nextLine();
+      double height = Double.parseDouble(fileScanner.next());
+      double weight = DOuble.parseDouble(fileScanner.next());
       Scanner keyboard = new Scanner(System.in);
    
-      // Have the user input information   
-      System.out.print("Please enter the Policy Number: ");
-      policyNumber = keyboard.nextLine();
-            
-      System.out.print("Please enter the Provider Name: ");
-      providerName = keyboard.nextLine();
+      Policy policy = new Policy(policyNumber,providerName, firstName, lastName, age, smokingStatus, height, weight);
+      policies.add(policy);
+  }
+}     catch (FileNotFoundException e) {
+         System.out.println("ERROR: PolicyInformation.txt not found.");
+         return;
+  }
       
-      System.out.print("Please enter the Policyholder's First Name: ");
-      firstName = keyboard.nextLine();
-      
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      lastName = keyboard.nextLine();
-      
-      System.out.print("Please enter the Policyholder's Age: ");
-      age = keyboard.nextInt();
-      keyboard.nextLine();
-      
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      smokingStatus = keyboard.nextLine();
-      
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      height = keyboard.nextDouble();
-      
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      weight = keyboard.nextDouble();
-      
-      Policy policy = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
-      
-      System.out.println();
-      
-      // Display info about the Policy
-      System.out.println("Policy Number: " + policy.getPolicyNumber());
-      System.out.println("Provider Name: " + policy.getProviderName());
-      System.out.println("Policyholder's First Name: " + policy.getFirstName());
-      System.out.println("Policyholder's Last Name: " + policy.getLastName());
-      System.out.println("Policyholder's Age: " + policy.getAge());
-      System.out.println("Policyholder's Smoking Status: " + policy.getSmokingStatus());
-      System.out.println("Policyholder's Height: " + policy.getHeight() + " inches");
-      System.out.println("Policyholder's Weight: " + policy.getWeight() + " pounds");
-      System.out.printf("Policyholder's BMI: %.2f\n", policy.getBMI());
-      System.out.printf("Policy Price: $%.2f\n", policy.getPrice());
+      // Display info about the Policies
+      for (Policy policy : policies) {
+         System.out.println(policy);
+      }
    }
 }
