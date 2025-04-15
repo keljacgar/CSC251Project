a073d7ac16e109ad08edf3c64ecf97d2407e013b
Kelly <kgarci50@lab.cpcc.edu>
2025-04-14 22:54:41 -0400

    Update Policy.java
    Moved fields and methods to the PolicyHolder class

--- Policy.java
+++ Policy.java
@@ -2,12 +2,8 @@ public class Policy
 {  // Fields
    private String policyNumber;
    private String providerName;
-   private String firstName;
-   private String lastName;
-   private int age;
-   private String smokingStatus;
-   private double height;
-   private double weight;
+   private PolicyHolder policyHolder; 
+   private static int policyCount = 0;
 
    /* Default constructor for policy
    *  Intializes fields with default values
@@ -16,83 +12,47 @@ public class Policy
    {
       policyNumber = "";
       providerName = "";
-      firstName = "";
-      lastName = "";
-      age = 0;
-      smokingStatus = "";
-      height = 0;
-      weight = 0;
+      policyHolder = new PolicyHolder();
+      policyCount++;
    }
    
-   /* Gets the policy number
-   *  @return the policy number as a string
-   */
-   public String getPolicyNumber()
-   {
-      return policyNumber;
-   }
-   /** Retrieves the provider's name
-   *   @return the provider's name as a string
-   */
-   public String getProviderName()
-   {
-      return providerName;
-   }
-   /** Retrieves the policy holder's first name.
-   *   @return the first name of the policy holder as a string
-   */
-   public String getFirstName()
-   {
-      return firstName;
-   }
-   /** Retrieves the policy holder's last name.
-   *   @return the last name of the policy holder as a string
-   */
-   public String getLastName()
-   {
-      return lastName;
-   }
-   
-   /** Retrieves policy holder's age
-   *   @return the age of the policyholder as an integer
-   */
-   public int getAge()
-   {
-      return age;
-   }
-   /** Retrieves the policy holder smoking status
-   *   @return the smoking status of the policy holder as a string
-   */
-   public String getSmokingStatus()
-   {
-      return smokingStatus;
-   }
-   
-   /** Retrieves the policy holder heigh in inches
-   *   @return the height of the policy holder as a double
-   */
-   public double getHeight()
-   {
-      return height;
-   }
-   
-   /** Policy holders weight in pounds
-   *   @return the weight of the policy holder as a double
-   */
-   public double getWeight()
-   {
-      return weight;
-   }
-   
-   /** Calculates BMI for the policy holder
-   *   @return the BMI value as a double
-   */
-      public double getBMI()
-   {
-      final double CONVFACTOR = 703;
-      
-      return (weight * CONVFACTOR) / (height * height);
-   }
+     /**
+     * parameterized constructor for policy.
+     * @param policyNumber  
+     * @param providerName  
+     * @param policyHolder  
+     */
+    public Policy(String policyNumber, String providerName, PolicyHolder policyHolder) {
+        this.policyNumber = policyNumber;
+        this.providerName = providerName;
+        this.policyHolder = policyHolder;
+        policyCount++;
+    }
+
+    // Getter & Setter methods
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
+    public PolicyHolder getPolicyHolder() {
+        return policyHolder;
+    }
+
+    public void setPolicyHolder(PolicyHolder policyHolder) {
+        this.policyHolder = policyHolder;
+    }
    
    /** Calculates the price of the policy based off age, smoking status, & BMI.
    *   @return the price of the policy as a double
@@ -120,3 +80,24 @@ public class Policy
          
       return price;
    }
+
+     /**
+     * returns a string representation of the policy.
+     * @return a formatted string containing the policy's info
+     */
+    @Override
+    public String toString() {
+        return "Policy Number: " + policyNumber +
+               "\nProvider Name: " + providerName +
+               "\n" + policyHolder.toString() +
+               "\nPolicy Price: $" + String.format("%.2f", getPrice());
+    }
+
+    /**
+     * Returns the total number of Policy objects created.
+     * @return The total number of Policy objects created.
+     */
+    public static int getPolicyCount() {
+        return policyCount;
+    }
+}
